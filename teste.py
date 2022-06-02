1ca2378f4b9a69dba962490cbb88ed07097c2e8f
Joao Victor <ovictorjo23@gmail.com>
2022-06-02 00:26:27 -0300

    minimizando guia chrome

--- teste.py
+++ teste.py
@@ -1,5 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument('--headless')
 
 s = Service("./chromedriver_linux64/chromedriver")
-driver = webdriver.Chrome(service=s)
+driver = webdriver.Chrome(service=s, options=options)
+
+
